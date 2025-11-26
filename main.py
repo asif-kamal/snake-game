5ddfc0c531fd4db1fd171ebe6cc699493d6913d4
asif-kamal <asif.kamal1489@gmail.com>
2025-11-25 20:04:54 -0600

    built snake game - moved all 3 squares seamlessly

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 from turtle import Turtle, Screen
 
 screen = Screen()
@@ -5,7 +6,10 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
 
+segments = []
 x = 0
+screen.tracer(0)
+
 for _ in range(3):
     t = Turtle()
     t.penup()
@@ -13,8 +17,15 @@ for _ in range(3):
     t.setpos(x, 0)
     t.turtlesize(stretch_len=0.6, stretch_wid=0.6, outline=1)
     t.color("white", "white")
+    segments.append(t)
     x -= 13
 
-
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    for index in range(len(segments) - 1, 0, -1):
+        segments[index].goto(segments[index - 1].xcor(), segments[index - 1].ycor())
+    segments[0].forward(10)
 
 screen.exitonclick()
