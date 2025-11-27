82ae7539bffdcb7eb0fd5a815732670942ec8ed3
asif-kamal <asif.kamal1489@gmail.com>
2025-11-27 15:24:23 -0600

    built snake game - added directionality to snake via key presses

--- snake.py
+++ snake.py
@@ -25,25 +25,39 @@ class Snake:
             for index in range(len(self.segments) - 1, 0, -1):
                 self.segments[index].goto(self.segments[index - 1].xcor(), self.segments[index - 1].ycor())
 
-            if self.segments[0].xcor() == 270 and self.segments[0].ycor() == 270:
+            if self.segments[0].xcor() == 270 and self.segments[0].ycor() == 270 and self.segments[1].heading == 270:
                 self.segments[0].left(0)
                 self.segments[0].forward(PACES)
             elif self.segments[0].xcor() == -270 and self.segments[0].ycor() == -270:
                 self.segments[0].left(0)
                 self.segments[0].forward(PACES)
-            elif self.segments[0].xcor() == 270:
+            if self.segments[0].xcor() == 270 and self.segments[0].heading() == 0:
                 self.segments[0].left(90)
                 self.segments[0].forward(PACES)
-            elif self.segments[0].xcor() == -270:
+            elif self.segments[0].xcor() == -270 and self.segments[0].heading() == 0:
                 self.segments[0].left(0)
                 self.segments[0].forward(PACES)
-            elif self.segments[0].ycor() == 270:
+            elif self.segments[0].ycor() == 270 and self.segments[0].heading() == 0:
                 self.segments[0].left(0)
                 self.segments[0].forward(PACES)
-            elif self.segments[0].ycor() == -270:
+            elif self.segments[0].ycor() == -270 and self.segments[0].heading() == 0:
                 self.segments[0].left(180)
                 self.segments[0].forward(PACES)
             else:
                 self.segments[0].forward(PACES)
+
+
+
+    def up(self):
+        self.segments[0].setheading(90)
+
+    def down(self):
+        self.segments[0].setheading(270)
+
+    def left(self):
+        self.segments[0].setheading(180)
+
+    def right(self):
+        self.segments[0].setheading(0)
 
 #write the rest of the program by yourself
